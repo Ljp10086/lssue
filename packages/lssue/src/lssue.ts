581273c39b4d388b50e0a5131b3e2cb83e9911ce
鲁举佩 <653390161@qq.com>
2023-04-07 10:35:43 +0800

    feat(lssue): extend event emitter

--- packages/lssue/src/lssue.ts
+++ packages/lssue/src/lssue.ts
@@ -1,10 +1,12 @@
 import { processOpts } from './helper';
 import { LssueOptions } from './types';
+import EventEmitter from './utils/events';
 
-export default class Lssue {
+export default class Lssue extends EventEmitter {
   opts: LssueOptions;
 
   constructor(opts: LssueOptions) {
+    super();
     this.opts = processOpts(opts);
   }
 
