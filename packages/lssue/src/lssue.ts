5a627650361b9ebf86e8e2af0f497d930c70d25e
鲁举佩 <653390161@qq.com>
2023-04-07 10:44:08 +0800

    feat(lssue): add mount function

--- packages/lssue/src/lssue.ts
+++ packages/lssue/src/lssue.ts
@@ -4,11 +4,29 @@ import EventEmitter from './utils/events';
 
 export default class Lssue extends EventEmitter {
   opts: LssueOptions;
+  isMounted = false;
+  container: Element | null = null;
+  el: HTMLElement;
 
   constructor(opts: LssueOptions) {
     super();
     this.opts = processOpts(opts);
+    this.el = document.createElement('div');
   }
 
-  // mount() {}
+  mount(container: string | HTMLElement): boolean {
+    if (this.isMounted) return true;
+    const el =
+      typeof container === 'string'
+        ? document.querySelector(container)
+        : container;
+
+    if (!el) return false;
+
+    el.appendChild(this.el);
+    this.container = el;
+    this.isMounted = true;
+
+    return true;
+  }
 }
